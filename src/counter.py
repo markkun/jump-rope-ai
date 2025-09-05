63148adf85b2e23bb13026f8d2a4067012df2f66
markkun <33755130+markkun@users.noreply.github.com>
2025-09-05 13:48:00 +0800

    Update counter.py

--- src/counter.py
+++ src/counter.py
@@ -1,10 +1,1 @@
-# src/counter.py
-import numpy as np
-from scipy.signal import find_peaks
 
-def count_jumps(pose_data, fps=30, min_interval=0.4):
-    hip_y = (pose_data[:,11,1] + pose_data[:,12,1]) / 2
-    signal = -hip_y
-    signal = (signal - signal.min()) / (signal.max() - signal.min() + 1e-6)
-    peaks, _ = find_peaks(signal, distance=int(fps*min_interval), prominence=0.1)
-    return len(peaks), peaks, signal
